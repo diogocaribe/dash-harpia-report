32f4fa5524c1791fd9a6b7fca320760a70519b77
Diogo Caribé <dogosousa@gmail.com>
2023-05-19 20:26:14 +0000

    Adicionando metodo que retorna geojson do banco de dados

--- app/config_db/repository/monitoramento_dissolve_repository.py
+++ app/config_db/repository/monitoramento_dissolve_repository.py
@@ -5,15 +5,17 @@ from sqlalchemy import func
 
 import geopandas as gpd
 
+
 class MonitoramentoDissolveRepository:
     def select_all(self):
         with DBConnectionHandler() as db:
             try:
-                data = data = db.session.query(func.ST_AsGeoJSON(MonitoramentoDissolve)).all()
+                data = db.session.query(func.ST_AsGeoJSON(MonitoramentoDissolve)).all()
                 return data
             except Exception as exception:
                 db.session.rollback()
                 raise exception
+
 
     def gdf_select_all(self, epsg=4674):
         """
@@ -29,11 +31,52 @@ class MonitoramentoDissolveRepository:
             try:
                 # data = db.session.query(MonitoramentoDissolve).all()
                 data = gpd.GeoDataFrame.from_postgis(
-                            sql='SELECT id, class_name, view_date, area_ha, geom FROM monitoramento_dissolve;',
-                            con=db.get_engine(),
-                            crs=epsg,
-                            index_col=["view_date"])
+                    sql="SELECT id, class_name, view_date, area_ha, geom FROM monitoramento_dissolve;",
+                    con=db.get_engine(),
+                    crs=epsg,
+                    index_col=["view_date"],
+                )
                 return data
             except Exception as exception:
                 db.session.rollback()
                 raise exception
+
+
+    def select_all_geojson(self):
+        """
+            Consulta que retorna geojson que é lido pelo leaflet.
+            SELECT row_to_json(fc)
+            FROM (
+                SELECT 'FeatureCollection' AS type, array_to_json(array_agg(f)) AS features
+                FROM (SELECT 'Feature' As type, ST_AsGeoJSON(st_transform(lg.geom, 4326))::json As geometry, 
+            (
+                SELECT row_to_json(t) 
+                FROM (SELECT id, view_date, class_name, area_ha) t
+            )
+            As properties
+            FROM monitoramento_dissolve As lg) As f)  As fc;
+        """
+        with DBConnectionHandler() as db:
+            try:
+                sql = """
+                    SELECT row_to_json(fc)
+                    FROM (
+                        SELECT 'FeatureCollection' AS type, array_to_json(array_agg(f)) AS features
+                        FROM (SELECT 'Feature' As type, ST_AsGeoJSON(st_transform(lg.geom, 4326))::json As geometry, 
+                    (
+                        SELECT row_to_json(t) 
+                        FROM (SELECT id, view_date, class_name, area_ha) t
+                    )
+                    As properties
+                    FROM monitoramento_dissolve As lg) As f)  As fc;
+                """
+                con=db.get_engine()
+                data = con.execute(sql)
+
+                return data
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
+
+
+
