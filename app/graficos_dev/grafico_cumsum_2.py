7ba952604249cbadc5293312eae7c3b45387d2e9
Diogo Caribé de Sousa <dogosousa@gmail.com>
2023-06-02 18:18:05 +0000

    Limpando o código

--- app/graficos_dev/grafico_cumsum_2.py
+++ app/graficos_dev/grafico_cumsum_2.py
@@ -1,10 +1,7 @@
-from datetime import date, datetime
-
-import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-from data import df_decremento_municipio
+
+df_decremento_municipio = pd.read_csv('app/graficos_dev/data/decremento_municipio_202305291512.csv', index_col="view_date") 
 
 template_graph = {
     "layout": {
@@ -26,30 +23,29 @@ template_graph = {
 
 df_decremento_municipio.index = pd.to_datetime(df_decremento_municipio.index)
 
-# df_decremento_municipio.index.year.unique() > 2016
+df_decremento_municipio = df_decremento_municipio[["area_ha"]].groupby(df_decremento_municipio.index).sum()
 
-df = df_decremento_municipio[df_decremento_municipio.index.year> 2016]
+t1 = pd.DataFrame()
 
-t = df_decremento_municipio.sort_index()["area_ha"].groupby([df_decremento_municipio.index]).sum()
-t1 = t.groupby([t.index.year]).cumsum()
+for ano in df_decremento_municipio.index.year.unique()[
+    df_decremento_municipio.index.year.unique() > 2017
+]:  
 
+    df = df_decremento_municipio[df_decremento_municipio.index.year == ano]
+    df["year"] = df.index.year
+    df["timedelta"] = (df.index - (df.index.year.astype("str") + "-01-01").astype("datetime64[ns]"))/1000000
+    df["cumsum"]= df["area_ha"].cumsum()
 
-data_day = go.Scatter(
-    x=t.index,
-    y=t1,
-    mode='lines',
-)
-layout = go.Layout(
-    title="Desflorestamento por Tempo",
+    t1 = pd.concat([t1, df[["year", "timedelta", "cumsum"]]])
+
+fig = px.line(t1, x="timedelta", y="cumsum", color='year')
+
+fig.update_layout(title="Desflorestamento por Tempo",
     xaxis={"title": "Data"},
     yaxis={"title": "Área (ha)"},
-    xaxis_tickformat='%d-%m'
+    xaxis_tickformat = '%d-%m'
 )
 
-fig = go.Figure(data=data_day, layout=layout)
-
-
-# fig.update_layout(height=600, xaxis_tickformat='%d-%m')
 fig.update_xaxes(type='date')
 
 fig.show()
