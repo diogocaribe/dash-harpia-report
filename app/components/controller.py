793af57f1c04114bf4eac8b980de80a72ee69624
Diogo Caribé <dogosousa@gmail.com>
2023-06-16 18:33:02 +0000

    Adicionando label ao date picker.

--- app/components/controller.py
+++ app/components/controller.py
@@ -4,6 +4,8 @@ from dataset.data import max_date, min_date, year_start
 
 date_range_picker = dmc.DateRangePicker(
     id="date-picker-range",
+    label="Intervalo de data",
+    description="Selecione o intervalo temporal para análise",
     minDate=min_date,
     maxDate=max_date,
     value=[year_start, max_date],
