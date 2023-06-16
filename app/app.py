8a2bab6a99d198316bc46ebc9a7c0678ec8443dd
Diogo Caribé <dogosousa@gmail.com>
2023-06-16 18:03:09 +0000

    Atualizando o repositorio

--- app/app.py
+++ app/app.py
@@ -12,3 +12,17 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 server = Flask(__name__)
 app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
 app.title = 'HarpiaDashboard'
+"""Dashboard Harpia"""
+
+# coding: utf-8
+
+import dash
+import dash_bootstrap_components as dbc
+from flask import Flask
+
+# css para deixar o layout bonito
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+server = Flask(__name__)
+app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
+app.title = 'HarpiaDashboard'
